4ab8159d92c5a6892ab1020efce25cf793902691
segwuonwu <segwuonwu@gmail.com>
2020-02-02 22:41:00 -0800

    Created game board

---
+++ main.js
@@ -0,0 +1,21 @@
+// Variable declaration
+var score = document.getElementById('top-left');
+var lives = document.getElementById('top-right');
+var game = document.getElementById('game');
+var btn = document.getElementById('status');
+
+let ctx = game.getContext('2d');
+
+// Set canvas width and height
+game.setAttribute("width", getComputedStyle(game)["width"]);
+game.setAttribute("height", getComputedStyle(game)["height"]);
+
+// function to draw the paddle
+function drawPaddle() {
+
+};
+
+// function to draw ball
+function drawBall() {
+
+};
